02034bf2d67f2ae62092b0688d81338f7b0788df
Tiger Wang <ziwei.tiger@hotmail.co.uk>
2013-07-29 10:40:57 +0100

    Implemented Issue #5 Enhancement [SEE DESC]
    Made spawn protection configurable in each world's world file. World has
    to be listed in settings.ini!

--- onbreakplaceblock.lua
+++ onbreakplaceblock.lua
@@ -4,10 +4,45 @@ function OnPlayerPlacingBlock(Player, BlockX, BlockY, BlockZ, BlockFace, CursorX
 		return false
 	end
 
-	if( Player:HasPermission("core.build") == false ) then
+    local Data = {} --(ARRAY TO HOLD SPAWNPROTECT DATA OF CHOSEN WORLD)
+    local PlayerWorld
+    local PROTECTRADIUS
+    local SPAWNPROTECT
+    local CountRadius = 1 --(WHERE THE STRING SPLITTER IS AT)
+    local CountProtect = 1 --(WHERE THE STRING SPLITTER IS AT)
+
+    for x = 1,#WorldsSpawnProtect do --(RUN THE LOOP HOWEVER MANY TIME THERE ARE WORLDS, UNLESS STOPPED)
+        for i in string.gmatch(WorldsSpawnProtect[x], "%S+") do --(SPLIT AWAY WORLDNAME IN STRING)
+            PlayerWorld = i
+            break    
+        end
+        if PlayerWorld == Player:GetWorld():GetName() then --(IF SPLIT WORLD IS PLAYER WORLD...)
+            for i in string.gmatch(WorldsSpawnProtect[x], "%S+") do --(...SPLIT AWAY PROTECTION RADIUS...)
+                Data[2] = i
+                PROTECTRADIUS = Data[2]
+                if CountRadius == 2 then break else --(IF STRING SPLITTER IS AT RIGHT PLACE THEN BREAK, ELSE CARRY ON)
+                    CountRadius = CountRadius + 1
+                end
+            end
+            for i in string.gmatch(WorldsSpawnProtect[x], "%S+") do --(...AND SPLIT AWAY TRUE/FALSE STRING (NOT BOOLEAN :( ))
+                Data[3] = i
+                SPAWNPROTECT = Data[3]
+                if CountProtect == 3 then break else --(IF STRING SPLITTER IS AT RIGHT PLACE THEN BREAK, ELSE CARRY ON)
+                    CountProtect = CountProtect + 1
+                end
+            end
+            break
+        end
+    end
+    LOGINFO(SPAWNPROTECT)
+    LOGINFO(PROTECTRADIUS)
+    LOGINFO(WorldsSpawnProtect[1])
+    LOGINFO(WorldsSpawnProtect[2])
+
+	if not (Player:HasPermission("core.build")) then
 		return true
 	else
-        if Player:HasPermission("core.spawnprotect.bypass") == false and SPAWNPROTECT == true then
+        if not (Player:HasPermission("core.spawnprotect.bypass")) and SPAWNPROTECT == "true" then
     	    local World = Player:GetWorld()
             local xcoord = World:GetSpawnX()
             local ycoord = World:GetSpawnY()
@@ -83,10 +118,10 @@ function OnPlayerBreakingBlock(Player, BlockX, BlockY, BlockZ, BlockFace, Status
     -- dont check if the direction is in the air
 	if (BlockFace ~= -1) then
 
-		if (Player:HasPermission("core.build") == false) then
+		if not (Player:HasPermission("core.build")) then
 			return true
 		else
-            if Player:HasPermission("core.spawnprotect.bypass") == false and SPAWNPROTECT == true then
+            if not (Player:HasPermission("core.spawnprotect.bypass")) and SPAWNPROTECT == "true" then
     	        local World = Player:GetWorld()
 	            local xcoord = World:GetSpawnX()
 	            local ycoord = World:GetSpawnY()
