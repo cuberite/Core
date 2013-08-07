7fc56322420c0f99c1d0f0e6ba5df6169ef57fcc
Alexander Harkness <bearbin@gmail.com>
2013-08-07 16:45:09 +0100

    Removed some extra semicolons.

--- give.lua
+++ give.lua
@@ -14,38 +14,38 @@ function HandleGiveCommand(Split, Player)
 		local FoundItem = StringToItem(Split[3], Item)
 	end	
 	if not IsValidItem(Item.m_ItemType) then  -- StringToItem does not check if item is valid
-		FoundItem = false;
+		FoundItem = false
 	end
 
 	if not FoundItem  then
 		Player:SendMessage(cChatColor.Rose .. "[INFO] " .. cChatColor.White .. "Invalid item id or name!" )
-		return true;
+		return true
 	end
 
 	-- Work out how many items the user wants.
-	local ItemAmount = 1;
+	local ItemAmount = 1
 	if #Split > 3 then
-		ItemAmount = tonumber(Split[4]);
+		ItemAmount = tonumber(Split[4])
 		if ItemAmount == nil or ItemAmount < 1 or ItemAmount > 512 then
 			Player:SendMessage(cChatColor.Rose .. "[INFO] " .. cChatColor.White .. "Invalid amount!" )
 			return true
 		end
 	end
 
-	Item.m_ItemCount = ItemAmount;
+	Item.m_ItemCount = ItemAmount
 
 	-- Get the playername from the split.
 	local playerName = Split[2]
 
 	local function giveItems(newPlayer)
-		local ItemsGiven = newPlayer:GetInventory():AddItem(Item);
+		local ItemsGiven = newPlayer:GetInventory():AddItem(Item)
 		if ItemsGiven == ItemAmount then
 			newPlayer:SendMessage(cChatColor.Green .. "[INFO] " .. cChatColor.White .. "There you go!" )
 			if not newPlayer == Player then
 				Player:SendMessage(cChatColor.Green .. "[INFO] " .. cChatColor.White .. "Items given!" )
 			LOG("Gave " .. newPlayer:GetName() .. " " .. Item.m_ItemCount .. " times " .. Item.m_ItemType .. ":" .. Item.m_ItemDamage)
 		else
-			Player:SendMessage( cChatColor.Rose .. "[INFO] " .. cChatColor.White .. "Not enough space in inventory, only gave " .. ItemsGiven);
+			Player:SendMessage( cChatColor.Rose .. "[INFO] " .. cChatColor.White .. "Not enough space in inventory, only gave " .. ItemsGiven)
 			LOG( "Player " .. newPlayer:GetName() .. " asked for " .. Item.m_ItemCount .. " times " .. Item.m_ItemType .. ":" .. Item.m_ItemDamage ..", but only could fit " .. ItemsGiven )
 		end
 		return true
