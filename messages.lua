150c88ccfe2d6e5210f95c31e56f824832a0c98c
Alexander Harkness <bearbin@gmail.com>
2013-08-10 18:36:14 +0100

    Added spaces after the tag for the messages so the results look good.

--- messages.lua
+++ messages.lua
@@ -4,7 +4,7 @@ g_UsePrefixes = true
 
 function SendMessage(a_Player, a_Message)
 	if (g_UsePrefixes) then
-		a_Player:SendMessage(cChatColor.Yellow .. "[INFO]" .. cChatColor.White .. a_Message)
+		a_Player:SendMessage(cChatColor.Yellow .. "[INFO] " .. cChatColor.White .. a_Message)
 	else
 		a_Player:SendMessage(cChatColor.Yellow .. a_Message)
 	end
@@ -12,7 +12,7 @@ end
 
 function SendMessageSuccess(a_Player, a_Message)
 	if (g_UsePrefixes) then
-		a_Player:SendMessage(cChatColor.Green .. "[INFO]" .. cChatColor.White .. a_Message)
+		a_Player:SendMessage(cChatColor.Green .. "[INFO] " .. cChatColor.White .. a_Message)
 	else
 		a_Player:SendMessage(cChatColor.Green .. a_Message)
 	end
@@ -20,7 +20,7 @@ end
 
 function SendMessageFailure(a_Player, a_Message)
 	if (g_UsePrefixes) then
-		a_Player:SendMessage(cChatColor.Red .. "[INFO]" .. cChatColor.White .. a_Message)
+		a_Player:SendMessage(cChatColor.Red .. "[INFO] " .. cChatColor.White .. a_Message)
 	else
 		a_Player:SendMessage(cChatColor.Red .. a_Message)
 	end
