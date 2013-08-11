1e4676965a3ce6dc79912e0c8686b7de5026eb42
tonibm19 <tonibm19@gmail.com>
2013-08-11 13:32:04 +0200

    Added /weather [Clear/Rain/Thunder]

--- weather.lua
+++ weather.lua
@@ -8,3 +8,25 @@ function HandleDownfallCommand( Split, Player )
 	Player:SendMessage(cChatColor.Green .. "[INFO] " .. cChatColor.White .. "Downfall toggled")
 	return true
 end
+
+function HandleWeatherCommand(Split, Player)
+	if( #Split ~= 2 ) then
+		Player:SendMessage( cChatColor.Green .. "Usage: /weather [Sun/Rain/Thunder]" )
+		return true;
+	end
+
+	local Server = cRoot:Get():GetServer()
+	if (Split[2] == "clear") then
+		Player:GetWorld():SetWeather(0)
+		Server:SendMessage( cChatColor.Green .. Player:GetName() .. " set the weather to Clear.")
+	elseif (Split[2] == "rain") then
+		Player:GetWorld():SetWeather(1)
+		Server:SendMessage( cChatColor.Green .. Player:GetName() .. " set the weather to Rain.")
+	elseif (Split[2] == "thunder") then
+		Player:GetWorld():SetWeather(2)
+		Server:SendMessage( cChatColor.Green .. Player:GetName() .. " set the weather to Thunder.")
+      else
+		Player:SendMessage( cChatColor.Green .. "Usage: /weather [Clear/Rain/Thunder]" )
+	end
+	return true
+end
