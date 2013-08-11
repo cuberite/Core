dc5ce476e871b425f79e510522e35db5608161d4
Tiger Wang <ziwei.tiger@hotmail.co.uk>
2013-08-11 14:52:40 +0100

    Now conforms to chat standards

--- weather.lua
+++ weather.lua
@@ -1,32 +1,19 @@
-function HandleDownfallCommand( Split, Player )
-	World = Player:GetWorld()
-	if World:GetWeather() == 0 then
-		World:SetWeather(1)
-	else
-		World:SetWeather(0)
-	end
-	Player:SendMessage(cChatColor.Green .. "[INFO] " .. cChatColor.White .. "Downfall toggled")
-	return true
-end
-
 function HandleWeatherCommand(Split, Player)
 	if( #Split ~= 2 ) then
-		Player:SendMessage( cChatColor.Green .. "Usage: /weather [Sun/Rain/Thunder]" )
+		Player:SendMessage(cChatColor.Yellow .. "[INFO] " .. "Usage: /weather [clear/rain/thunder]" )
 		return true;
 	end
 
-	local Server = cRoot:Get():GetServer()
 	if (Split[2] == "clear") then
 		Player:GetWorld():SetWeather(0)
-		Server:SendMessage( cChatColor.Green .. Player:GetName() .. " set the weather to Clear.")
+		Player:SendMessage(cChatColor.Green .. "[INFO] " .. "Downfall stopped" )
 	elseif (Split[2] == "rain") then
 		Player:GetWorld():SetWeather(1)
-		Server:SendMessage( cChatColor.Green .. Player:GetName() .. " set the weather to Rain.")
+		Player:SendMessage(cChatColor.Green .. "[INFO] " .. "Let it rain!" )
 	elseif (Split[2] == "thunder") then
 		Player:GetWorld():SetWeather(2)
-		Server:SendMessage( cChatColor.Green .. Player:GetName() .. " set the weather to Thunder.")
-      else
-		Player:SendMessage( cChatColor.Green .. "Usage: /weather [Clear/Rain/Thunder]" )
+		Player:SendMessage(cChatColor.Green .. "[INFO] " .. "Thundery showers activate!")
 	end
+	
 	return true
 end
