fb96b73831679735a9677d7f520d19d8276a15d6
tonibm19 <tonibm19@gmail.com>
2013-08-11 13:33:49 +0200

    Added /weather [Clear/Rain/Thunder]

--- main.lua
+++ main.lua
@@ -58,6 +58,7 @@ function Initialize(Plugin)
 	PluginManager:BindCommand("/tpaccept",        "core.teleport",        HandleTPAcceptCommand,        " ~ Accept a teleportation request");
 	PluginManager:BindCommand("/unban",           "core.unban",           HandleUnbanCommand,           " ~ Unban a player");
 	PluginManager:BindCommand("/viewdistance",    "core.viewdistance",    HandleViewDistanceCommand,    " [".. cClientHandle.MIN_VIEW_DISTANCE .."-".. cClientHandle.MAX_VIEW_DISTANCE .."] - Change your view distance")
+	PluginManager:BindCommand("/weather",         "core.weather",         HandleWeatherCommand,         " - Change world weather");
 	PluginManager:BindCommand("/worlds",          "core.worlds",          HandleWorldsCommand,          " - Shows a list of all the worlds");
 
 	InitConsoleCommands();
