b84ed93a5d359b806f868b5cb56bea8524e2df8f
Alexander Harkness <bearbin@gmail.com>
2013-08-16 09:18:12 +0100

    Added some more translations.

--- languages/en.lua
+++ languages/en.lua
@@ -1,4 +1,13 @@
 -- The english language file for MCServer
 
-g_Languages['en']['go-further-from-spawn'] = "Go further from spawn to build"
+g_Languages['en']['go-further-from-spawn'] = "Go further from spawn to build!"
 g_Languages['en']['core-disabled'] = "Disabled Core!"
+g_Languages['en']['no-known-last-pos'] = "No known last position!"
+g_Languages['en']['back-teleported'] = "Teleported back to your last known position."
+g_Languages['en']['ban-usage'] = "Usage: /ban <player> [reason]"
+g_Languages['en']['banplayer-not-found'] = "Could not find player, but banned anyway."
+g_Languages['en']['banplayer-success'] = "Successfully kicked and banned player."
+g_Languages['en']['unban-usage'] = "Usage: /unban [Player]"
+g_Languages['en']['unban-player-not-found'] = "Player is not banned!"
+g_Languages['en']['unbanning'] = "Unbanning "
+g_Languages['en']['isunbanning'] = " is unbanning "
