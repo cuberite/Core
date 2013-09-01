9b234533c6f83ac500cfae564d5356a2767b967c
tonibm19 <tonibm19@gmail.com>
2013-09-01 20:19:04 +0200

    fixed errors

--- time.lua
+++ time.lua
@@ -9,17 +9,22 @@ function HandleTimeCommand( Split, Player )
 	if string.upper( Split[2] ) == "DAY" then
 		Player:GetWorld():SetTimeOfDay( 0 )
 		Server:SendMessage(cChatColor.Green .. "[INFO] " .. cChatColor.White .. "Time was set to daytime" )
+		return true
 	elseif string.upper( Split[2] ) == "NIGHT" then
 		Player:GetWorld():SetTimeOfDay( 12000 + 1000 )
 		Server:SendMessage( cChatColor.Green .. "[INFO] " .. cChatColor.White .. "Time was set to night time" )
+		return true
 	elseif string.upper( Split[2] ) == "SET" and tonumber( Split[3] ) ~= nil then
 		Player:GetWorld():SetTimeOfDay( tonumber(Split[3]) )
 		Server:SendMessage( cChatColor.Green .. "[INFO] " .. cChatColor.White .. "Time was set to " .. Split[3] )
+		return true
 	elseif string.upper( Split[2] ) == "ADD" and tonumber( Split[3] ) ~= nil then
 		Player:GetWorld():SetTimeOfDay( Player:GetWorld():GetTimeOfDay() + Split[3] )
 		Server:SendMessage( cChatColor.Green .. "[INFO] " .. cChatColor.White .. Split[3] .. "Was added to the time" )
+		return true
 	else
 		SendMessage( Player, "Usage: /time [Day/Night/Set/Add]" )
+		return true
 	end
 	return true
 
