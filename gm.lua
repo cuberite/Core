733738ac679c276b36c375efdba9310df31b22f0
tonibm19 <tonibm19@gmail.com>
2013-11-04 11:23:04 +0100

    /gm rewrite to addapt to vanilla
    Now you can change other player's gamemode.
    Gamemode can be specified with name too.

--- gm.lua
+++ gm.lua
@@ -1,15 +1,49 @@
 function HandleChangeGMCommand( Split, Player )
-
-	if( #Split ~= 2 ) then
-		SendMessage( Player, "Usage: /gm [0|1|2]" )
+	if( Split[2] == nil ) then
+		SendMessage( Player, "Usage: " ..Split[1].. " [survival|creative|adventure] [player] " )
 		return true
 	end
 
-	if ( Split[2] < "3" ) then
-                Player:SetGameMode( Split[2] )
-	        return true
-        else
-                SendMessage( Player, "Usage: /gm [0|1|2]" )
-                return true
-        end
+	if( Split[3] == nil ) then
+		if (Split[2] == "survival") or (Split[2] == "0") then
+            		Player:SetGameMode(0)
+            	elseif (Split[2] == "creative") or (Split[2] == "1") then
+			Player:SetGameMode(1)
+		elseif (Split[2] == "adventure") or (Split[2] == "2") then
+			Player:SetGameMode(2)
+		else
+			SendMessage( Player, "Usage: " ..Split[1].. " [survival|creative|adventure] [player] " )		
+		end
+            	return true
+	end
+
+	local IsPlayerOnline = false;
+	local ChangeGM = function(OtherPlayer)
+		if (OtherPlayer:GetName() == Split[3]) then
+			IsPlayerOnline = true
+            		if (Split[2] == "survival") or (Split[2] == "0") then
+				OtherPlayer:SetGameMode(0)
+            		elseif (Split[2] == "creative") or (Split[2] == "1") then
+				OtherPlayer:SetGameMode(1)
+			elseif (Split[2] == "adventure") or (Split[2] == "2") then
+				OtherPlayer:SetGameMode(2)
+			else
+				IsPlayerOnline = invalidgm
+            		end
+		end
+	end
+
+	cRoot:Get():FindAndDoWithPlayer(Split[3], ChangeGM);
+	if (IsPlayerOnline) then
+		SendMessage( Player, "Changed gamemode for player " .. Split[3] )
+		return true
+	end
+	if (IsPlayerOnline == invalidgm) then
+        	SendMessage( Player, "Usage: " ..Split[1].. " [survival|creative|adventure] [player] " )	
+		return true
+	end
+      	if (IsPlayerOnline == false) then
+        	SendMessageFailure( Player, "Player not found" )
+		return true
+	end
 end
