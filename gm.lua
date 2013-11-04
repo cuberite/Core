c0f26107abe1c277e3cae4fdb09df83b17c90ff1
tonibm19 <tonibm19@gmail.com>
2013-11-04 11:28:15 +0100

    Using tabs instead of spaces
    Fixed indentation

--- gm.lua
+++ gm.lua
@@ -6,30 +6,30 @@ function HandleChangeGMCommand( Split, Player )
 
 	if( Split[3] == nil ) then
 		if (Split[2] == "survival") or (Split[2] == "0") then
-            		Player:SetGameMode(0)
-            	elseif (Split[2] == "creative") or (Split[2] == "1") then
+			Player:SetGameMode(0)
+		elseif (Split[2] == "creative") or (Split[2] == "1") then
 			Player:SetGameMode(1)
 		elseif (Split[2] == "adventure") or (Split[2] == "2") then
 			Player:SetGameMode(2)
 		else
 			SendMessage( Player, "Usage: " ..Split[1].. " [survival|creative|adventure] [player] " )		
 		end
-            	return true
+		return true
 	end
 
 	local IsPlayerOnline = false;
 	local ChangeGM = function(OtherPlayer)
 		if (OtherPlayer:GetName() == Split[3]) then
 			IsPlayerOnline = true
-            		if (Split[2] == "survival") or (Split[2] == "0") then
+			if (Split[2] == "survival") or (Split[2] == "0") then
 				OtherPlayer:SetGameMode(0)
-            		elseif (Split[2] == "creative") or (Split[2] == "1") then
+			elseif (Split[2] == "creative") or (Split[2] == "1") then
 				OtherPlayer:SetGameMode(1)
 			elseif (Split[2] == "adventure") or (Split[2] == "2") then
 				OtherPlayer:SetGameMode(2)
 			else
 				IsPlayerOnline = invalidgm
-            		end
+			end
 		end
 	end
 
@@ -39,11 +39,11 @@ function HandleChangeGMCommand( Split, Player )
 		return true
 	end
 	if (IsPlayerOnline == invalidgm) then
-        	SendMessage( Player, "Usage: " ..Split[1].. " [survival|creative|adventure] [player] " )	
+		SendMessage( Player, "Usage: " ..Split[1].. " [survival|creative|adventure] [player] " )	
 		return true
 	end
-      	if (IsPlayerOnline == false) then
-        	SendMessageFailure( Player, "Player not found" )
+	if (IsPlayerOnline == false) then
+		SendMessageFailure( Player, "Player not found" )
 		return true
 	end
 end
