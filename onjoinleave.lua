f9799372c3240cb94f225fa2075d21e86e5c4613
Tiger Wang <ziwei.tiger@hotmail.co.uk>
2013-12-03 20:43:23 +0000

    Removed commented out code

--- onjoinleave.lua
+++ onjoinleave.lua
@@ -1,21 +1,10 @@
 function OnPlayerJoined(Player)
-	--if( BannedPlayersIni:GetValueB("Banned", Player:GetName(), false) == true ) then
-	--	LOGINFO( Player:GetName() .. " tried to join, but is banned!" )
-	--	KickPlayer(Player:GetName(), cChatColor.Red .. "You are banned!" )
-	--	return true
-	--elseif( WhiteListIni:GetValueB("WhiteListSettings", "WhiteListOn", false ) == true ) then
-	--	if( WhiteListIni:GetValueB("WhiteList", Player:GetName(), false ) == false ) then
-	--		LOGINFO( Player:GetName() .. " tried to join, but is not whitelisted!" )
-	--		KickPlayer(Player:GetName(), cChatColor.Red .. "You are not whitelisted!" )
-	--	end
-	--else
 	
 	ShowMOTDTo( Player )
 	cRoot:Get():BroadcastChat(cChatColor.Yellow .. "[JOIN] " .. cChatColor.White .. Player:GetName() .. " has joined the game" )
 	LOGINFO("Player " .. Player:GetName() .. " has joined the game." )
 	return false
 	
-	--end
 end
 
 function OnDisconnect(Player, Reason)
