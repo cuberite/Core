0ca82959bb831d708f8a193d6444efb8e8c38738
Tiger Wang <ziwei.tiger@hotmail.co.uk>
2013-12-03 14:20:47 +0000

    Fixed tabs in spawn.lua
    Brought messages up to standards.

--- spawn.lua
+++ spawn.lua
@@ -1,4 +1,5 @@
 function HandleSpawnCommand(Split, Player)
+	
 	SpawnX = WorldIni:GetValue("SpawnPosition", "X")
 	SpawnY = WorldIni:GetValue("SpawnPosition", "Y")
 	SpawnZ = WorldIni:GetValue("SpawnPosition", "Z")
@@ -12,19 +13,21 @@ function HandleSpawnCommand(Split, Player)
 end
 
 function HandleSetSpawnCommand(Split, Player)
+	
 	PlayerX = Player:GetPosX()
 	PlayerY = Player:GetPosY()
 	PlayerZ = Player:GetPosZ()
 	
-    WorldIni:DeleteValue("SpawnPosition", "X")
-    WorldIni:DeleteValue("SpawnPosition", "Y")
-    WorldIni:DeleteValue("SpawnPosition", "Z")
+	WorldIni:DeleteValue("SpawnPosition", "X")
+	WorldIni:DeleteValue("SpawnPosition", "Y")
+	WorldIni:DeleteValue("SpawnPosition", "Z")
 	
-    WorldIni:SetValue("SpawnPosition", "X", PlayerX)
-    WorldIni:SetValue("SpawnPosition", "Y", PlayerY)
-    WorldIni:SetValue("SpawnPosition", "Z", PlayerZ)
-    WorldIni:WriteFile()
+	WorldIni:SetValue("SpawnPosition", "X", PlayerX)
+	WorldIni:SetValue("SpawnPosition", "Y", PlayerY)
+	WorldIni:SetValue("SpawnPosition", "Z", PlayerZ)
+	WorldIni:WriteFile()
 	
-    SendMessageSuccess( Player, "Changed spawn position to " .. PlayerX .. ", " .. PlayerY .. ", " .. PlayerZ )
-    return true
+	SendMessageSuccess( Player, string.format("Changed spawn position to [X:%i Y:%i Z:%i]", PlayerX, PlayerY, PlayerZ) )
+	return true
+	
 end
