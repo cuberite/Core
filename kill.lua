de4069b17bc63f5a3f6531cd90dd03135899c26b
tonibm19 <tonibm19@gmail.com>
2014-01-13 21:23:55 +0100

    Now a player can be killed in creative mode.

--- kill.lua
+++ kill.lua
@@ -1,28 +1,19 @@
 function HandleKillCommand( Split, Player )
 
 	if (Split[2] == nil) then
-		Player:TakeDamage(dtPlugin, nil, 1000, 1000, 0)
+		Player:TakeDamage(dtInVoid, nil, 1000, 1000, 0)
 		return true
 	end
 
 	local HasKilled = false;
 	local KillPlayer = function(OtherPlayer)
 		if (OtherPlayer:GetName() == Split[2]) then
-				if (OtherPlayer:GetGameMode() == 1) then
-					HasKilled = creative
-				end
-				if (OtherPlayer:GetGameMode() == 0) then
-					OtherPlayer:TakeDamage(dtPlugin, nil, 1000, 1000, 0)
-					HasKilled = true
-				end
-		 end
+				OtherPlayer:TakeDamage(dtInVoid, nil, 1000, 1000, 0)
+				HasKilled = true
+		end
 	end
 
 	cRoot:Get():FindAndDoWithPlayer(Split[2], KillPlayer);
-	if (HasKilled == creative) then
-		SendMessageFailure( Player, "Player " .. Split[2] .. " is in creative mode" )
-		return true
-	end
 	if (HasKilled) then
 		SendMessageSuccess( Player, "Player " .. Split[2] .. " is killed" )
 		return true
