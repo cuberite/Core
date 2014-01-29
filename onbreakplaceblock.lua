152e40068e771daee88544cf6006d1815d041ace
Alexander Harkness <bearbin@gmail.com>
2014-01-29 21:15:34 +0000

    Update onbreakplaceblock.lua

--- onbreakplaceblock.lua
+++ onbreakplaceblock.lua
@@ -1,5 +1,5 @@
 function OnPlayerPlacingBlock(Player, BlockX, BlockY, BlockZ, BlockFace, CursorX, CursorY, CursorZ, BlockType)
-	-- Direction is air check
+	-- Don't fail if the block is air.
 	if (BlockFace == -1) then
 		return false
 	end
@@ -9,61 +9,61 @@ function OnPlayerPlacingBlock(Player, BlockX, BlockY, BlockZ, BlockFace, CursorX
 	if not (Player:HasPermission("core.build")) then
 		SendMessageFailure( Player, "You do not have the 'core.build' permission, thou cannot build" )
 		return true
-	else
-		if not (Player:HasPermission("core.spawnprotect.bypass")) and (PROTECTRADIUS ~= 0) then
-			local World = Player:GetWorld()
-			local xcoord = World:GetSpawnX()
-			local ycoord = World:GetSpawnY()
-			local zcoord = World:GetSpawnZ()
-
-			if not ((BlockX <= (xcoord + PROTECTRADIUS)) and (BlockX >= (xcoord - PROTECTRADIUS))) then
-				return false -- Not in spawn area.
-			end
-			if not ((BlockY <= (ycoord + PROTECTRADIUS)) and (BlockY >= (ycoord - PROTECTRADIUS))) then 
-				return false -- Not in spawn area.
-			end
-			if not ((BlockZ <= (zcoord + PROTECTRADIUS)) and (BlockZ >= (zcoord - PROTECTRADIUS))) then 
-				return false -- Not in spawn area.
-			end
-
-			SendMessageFailure( Player, "Go further from spawn to build" )
-
-			return true
-		end
 	end
+	
+	if not (Player:HasPermission("core.spawnprotect.bypass")) and (PROTECTRADIUS ~= 0) then
+		local World = Player:GetWorld()
+		local xcoord = World:GetSpawnX()
+		local ycoord = World:GetSpawnY()
+		local zcoord = World:GetSpawnZ()
+		if not ((BlockX <= (xcoord + PROTECTRADIUS)) and (BlockX >= (xcoord - PROTECTRADIUS))) then
+			return false -- Not in spawn area.
+		end
+		if not ((BlockY <= (ycoord + PROTECTRADIUS)) and (BlockY >= (ycoord - PROTECTRADIUS))) then 
+			return false -- Not in spawn area.
+		end
+		if not ((BlockZ <= (zcoord + PROTECTRADIUS)) and (BlockZ >= (zcoord - PROTECTRADIUS))) then 
+			return false -- Not in spawn area.
+		end
+			SendMessageFailure( Player, "Go further from spawn to build" )
+			return true
+	end
+	
 	return false
 end
 
 function OnPlayerBreakingBlock(Player, BlockX, BlockY, BlockZ, BlockFace, Status, OldBlockType, OldBlockMeta)
-	-- dont check if the direction is in the air
-	if (BlockFace ~= -1) then
+	-- Don't fail if the block is air.
+	if BlockFace == -1 then
+		return false
+	end
 
 	local PROTECTRADIUS = WorldsSpawnProtect[Player:GetWorld():GetName()];
 
-		if not (Player:HasPermission("core.build")) then
-			return true
-		else
-			if not (Player:HasPermission("core.spawnprotect.bypass")) and not (PROTECTRADIUS == 0) then
-				local World = Player:GetWorld()
-				local xcoord = World:GetSpawnX()
-				local ycoord = World:GetSpawnY()
-				local zcoord = World:GetSpawnZ()
+	if not (Player:HasPermission("core.build")) then
+		SendMessageFailure( Player, "You do not have the 'core.build' permission, thou cannot build" )
+		return true
+	end
 	
-				if not ((BlockX <= (xcoord + PROTECTRADIUS)) and (BlockX >= (xcoord - PROTECTRADIUS))) then
-					return false -- Not in spawn area.
-				end
-				if not ((BlockY <= (ycoord + PROTECTRADIUS)) and (BlockY >= (ycoord - PROTECTRADIUS))) then 
-					return false -- Not in spawn area.
-				end
-				if not ((BlockZ <= (zcoord + PROTECTRADIUS)) and (BlockZ >= (zcoord - PROTECTRADIUS))) then 
-					return false -- Not in spawn area.
-				end
-			
-				SendMessageFailure( Player, "Go further from spawn to build" )
+	if not (Player:HasPermission("core.spawnprotect.bypass")) and not (PROTECTRADIUS == 0) then
+		local World = Player:GetWorld()
+		local xcoord = World:GetSpawnX()
+		local ycoord = World:GetSpawnY()
+		local zcoord = World:GetSpawnZ()
 
-				return true
-			end
+		if not ((BlockX <= (xcoord + PROTECTRADIUS)) and (BlockX >= (xcoord - PROTECTRADIUS))) then
+			return false -- Not in spawn area.
 		end
+		if not ((BlockY <= (ycoord + PROTECTRADIUS)) and (BlockY >= (ycoord - PROTECTRADIUS))) then 
+			return false -- Not in spawn area.
+		end
+		if not ((BlockZ <= (zcoord + PROTECTRADIUS)) and (BlockZ >= (zcoord - PROTECTRADIUS))) then 
+			return false -- Not in spawn area.
+		end
+	
+		SendMessageFailure( Player, "Go further from spawn to build" )
+
+		return true
 	end
 
 	return false
