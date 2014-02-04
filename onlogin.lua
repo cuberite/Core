8aba76f8de6a8ffe59417378e2036d9253522bfd
Tiger Wang <ziwei.tiger@hotmail.co.uk>
2014-02-04 23:52:24 +0000

    Removed unused variable

--- onlogin.lua
+++ onlogin.lua
@@ -6,7 +6,6 @@ function OnLogin(Client, ProtocolVersion, Username)
 		end
 		if( WhiteListIni:GetValueB("WhiteListSettings", "WhiteListOn", false ) == true ) then
 			if( WhiteListIni:GetValueB("WhiteList", Username, false ) == false ) then -- not on whitelist
-				local Server = cRoot:Get():GetServer()
 				cRoot:Get():BroadcastChat(Username .. " tried to join, but is not on the whitelist.")
 				LOGINFO( Username .. " tried to join, but is not on the whitelist." )
 				return true -- Deny access to the server
