17b28fe797dc26c121acd3ea593bc55347d9ca34
Tiger Wang <ziwei.tiger@hotmail.co.uk>
2014-02-09 01:19:13 +0000

    Added an animation to /time

--- time.lua
+++ time.lua
@@ -4,28 +4,37 @@ function HandleTimeCommand( Split, Player )
 		SendMessage( Player, "Usage: /time [day/night] or [set/add] [amount]" )
 		return true
 	end
+	
+	local TimeToSet = 0
+	local CurrentTime = Player:GetWorld():GetTimeOfDay()
 
 	local Server = cRoot:Get():GetServer()
 	if string.upper( Split[2] ) == "DAY" then
-		Player:GetWorld():SetTimeOfDay( 0 )
 		cRoot:Get():BroadcastChat(cChatColor.Green .. "[INFO] " .. cChatColor.White .. "Time was set to daytime" )
-		return true
 	elseif string.upper( Split[2] ) == "NIGHT" then
-		Player:GetWorld():SetTimeOfDay( 12000 + 1000 )
+		TimeToSet = 12000 + 1000
 		cRoot:Get():BroadcastChat( cChatColor.Green .. "[INFO] " .. cChatColor.White .. "Time was set to night time" )
-		return true
 	elseif string.upper( Split[2] ) == "SET" and tonumber( Split[3] ) ~= nil then
-		Player:GetWorld():SetTimeOfDay( tonumber(Split[3]) )
+		TimeToSet = tonumber(Split[3])
 		cRoot:Get():BroadcastChat( cChatColor.Green .. "[INFO] " .. cChatColor.White .. "Time was set to " .. Split[3] )
-		return true
 	elseif string.upper( Split[2] ) == "ADD" and tonumber( Split[3] ) ~= nil then
-		Player:GetWorld():SetTimeOfDay( Player:GetWorld():GetTimeOfDay() + Split[3] )
-		cRoot:Get():BroadcastChat( cChatColor.Green .. "[INFO] " .. cChatColor.White .. Split[3] .. "Was added to the time" )
-		return true
+		TimeToSet = CurrentTime + Split[3]
+		cRoot:Get():BroadcastChat( cChatColor.Green .. "[INFO] " .. cChatColor.White .. Split[3] .. "was added to the time" )
 	else
 		SendMessage( Player, "Usage: /time [day/night] or [set/add] [amount]" )
 		return true
 	end
+	
+	if CurrentTime > TimeToSet then
+		for t = CurrentTime, TimeToSet, -2 do
+			Player:GetWorld():SetTimeOfDay(t)
+		end
+	else
+		for t = CurrentTime, TimeToSet, 2 do			
+			Player:GetWorld():SetTimeOfDay(t)
+		end
+	end
+	
 	return true
 
 end
