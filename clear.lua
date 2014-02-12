0e53588713215f0a9557ef55295e8aa22f265cba
tonibm19 <tonibm19@gmail.com>
2014-02-12 20:44:00 +0100

    Improved clear
    core.clear only allows to clear your own inventory.
    core.admin.clear allows you to clear any player's inventory

--- clear.lua
+++ clear.lua
@@ -1,24 +1,28 @@
 function HandleClearCommand( Split, Player )
+    if (Split[2] == nil) then
+        Player:GetInventory():Clear()
+        SendMessageSuccess( Player, "You cleared your own inventory" )
+        return true
+    else 
+        if Player:HasPermission("core.admin.clear") then
+            local InventoryCleared = false;
+            local ClearInventory = function(OtherPlayer)
+                if (OtherPlayer:GetName() == Split[2]) then
+                    OtherPlayer:GetInventory():Clear()
+                    InventoryCleared = true
+                end
+            end
 
-  if (Split[2] == nil) then
-     Split[2] = Player:GetName()
-  end
-
-  local InventoryCleared = false;
-  local ClearInventory = function(OtherPlayer)
-    if (OtherPlayer:GetName() == Split[2]) then
-      OtherPlayer:GetInventory():Clear()
-      InventoryCleared = true
+            cRoot:Get():FindAndDoWithPlayer(Split[2], ClearInventory);
+            if (InventoryCleared) then
+                SendMessageSuccess( Player, "You cleared the inventory of " .. Split[2] )
+                return true
+            else
+                SendMessageFailure( Player, "Player not found" )
+                return true
+            end
+        else
+            return false
+        end
     end
-  end
-
-  cRoot:Get():FindAndDoWithPlayer(Split[2], ClearInventory);
-  if (InventoryCleared) then
-    SendMessageSuccess( Player, "You cleared the inventory of " .. Split[2] )
-    return true
-  else
-    SendMessageFailure( Player, "Player not found" )
-    return true
-  end
-
 end
