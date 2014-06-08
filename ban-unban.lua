bdfd795b11ab6e25b34e7efc6d79228482b6af21
Alexander Harkness <bearbin@gmail.com>
2014-06-08 20:47:45 +0100

    Normalised formatting.

--- ban-unban.lua
+++ ban-unban.lua
@@ -1,45 +1,50 @@
-function HandleBanCommand( Split, Player )
+function HandleBanCommand(Split, Player)
 
-	if( #Split < 2 ) then
-		SendMessage( Player, "Usage: /ban [Player] <Reason>" )
+	if (#Split < 2) then
+		SendMessage(Player, "Usage: /ban [Player] <Reason>")
 		return true
 	end
 
-	local Reason = cChatColor.Red .. "You have been banned."
-	if( #Split > 2 ) then
-		Reason = table.concat( Split, " ", 3 )
+	-- If the player supplied a reason, use that, else use a default reason.
+	if (#Split > 2) then
+		local Reason = table.concat(Split, " ", 3)
+	else
+		local Reason = cChatColor.Red .. "You have been banned."
 	end
 
-	if KickPlayer(Split[2], Reason) == false then
-		BannedPlayersIni:SetValueB( "Banned", Split[2], true )
-		BannedPlayersIni:WriteFile( "banned.ini" )
-		SendMessageFailure( Player, "Could not find player, but banned anyway" )
+	-- Actually ban the player.
+	BannedPlayersIni:SetValueB("Banned", Split[2], true)
+	BannedPlayersIni:WriteFile("banned.ini")
+
+	-- Try akd kick the banned player, and send an appropriated response to the banner.
+	if (KickPlayer(Split[2], Reason)) then
+		SendMessageSuccess(Player, "Successfully kicked and banned " .. Split[2])
 	else
-		BannedPlayersIni:SetValueB( "Banned", Split[2], true )
-		BannedPlayersIni:WriteFile( "banned.ini" )
-		SendMessageSuccess( Player, "Successfully kicked and banned player" )
+		SendMessageFailure(Player, "Successfully Banned " .. Split[2])
 	end
+	
 	return true
 
 end
 
 function HandleUnbanCommand( Split, Player )
 
-	if( #Split < 2 ) then
-		SendMessage( Player, "Usage: /unban [Player]" )
+	if (#Split < 2) then
+		SendMessage(Player, "Usage: /unban [Player]")
 		return true
 	end
 
-	if( BannedPlayersIni:GetValueB("Banned", Split[2], false) == false ) then
-		SendMessageFailure( Player, "Player is not banned!" )
+	if (BannedPlayersIni:GetValueB("Banned", Split[2], false) == false) then
+		SendMessageFailure(Player, "Player is not banned!")
 		return true
 	end
 
+	-- Actually unban the banned player.
 	BannedPlayersIni:SetValueB("Banned", Split[2], false)
-	BannedPlayersIni:WriteFile( "banned.ini" )
+	BannedPlayersIni:WriteFile("banned.ini")
 
-	LOGINFO( Player:GetName() .. " is unbanning " .. Split[2] )
-	SendMessageSuccess( Player, "Unbanning " .. Split[2] )
+	LOGINFO(Player:GetName() .. " unbanned " .. Split[2])
+	SendMessageSuccess(Player, "Unbannex " .. Split[2])
 
 	return true
 
