f8d49795bf2eee3be8814c9815fb1d73155b97af
isavegas <isavegas@users.noreply.github.com>
2014-06-24 10:52:39 -0500

    Login rejection messages

--- onlogin.lua
+++ onlogin.lua
@@ -2,15 +2,17 @@ function OnLogin(Client, ProtocolVersion, Username)
 	if( Username ~= "" ) then
 		if( BannedPlayersIni:GetValueB("Banned", Username, false) == true ) then
 			LOGINFO( Username .. " tried to join, but is banned!")
+			Client:Kick("You are banned!")
 			return true -- Player is banned, return true to deny access
 		end
 		if( WhiteListIni:GetValueB("WhiteListSettings", "WhiteListOn", false ) == true ) then
 			if( WhiteListIni:GetValueB("WhiteList", Username, false ) == false ) then -- not on whitelist
 				cRoot:Get():BroadcastChat(Username .. " tried to join, but is not on the whitelist.")
 				LOGINFO( Username .. " tried to join, but is not on the whitelist." )
+				Client:Kick("You are not whitelisted on this server!")
 				return true -- Deny access to the server
 			end
 		end
-	end	
+	end
 	return false
 end
