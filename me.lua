60ed9566675a1aad5937a80c1143e2d8130dc621
isavegas <isavegas@users.noreply.github.com>
2014-06-28 16:16:48 -0500

    Streamlined /me
    Added the asterisk found in the "/me" command on the official Minecraft server implementation.
    
    Replaced the for loop concatenating the message with a Lua function call to achieve the same effect with less code.
    
    Completely redesigned the 'if' calls, to prevent unnecessary code from running.

--- me.lua
+++ me.lua
@@ -1,19 +1,11 @@
 function HandleMeCommand( Split, Player )
 
-	table.remove( Split, 1 )
-	local Message = ""
-
-	for i, Text in ipairs( Split ) do
-		Message = Message .. " " .. Text
-	end
-
-	if Split[1] == nil then
+	if not Split[2] then
 		SendMessage( Player, "Usage: /me <action>" )
 		return true
-	end
-
-	if Split[1] ~= nil then
-		cRoot:Get():BroadcastChat( Player:GetName() .. "" .. Message )
+	else
+		table.remove( Split, 1 )
+		cRoot:Get():BroadcastChat( "* " .. Player:GetName() .. " " .. table.concat( Split , " " ) )
 		return true
 	end
 
