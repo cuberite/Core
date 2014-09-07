ee6837c1548774ff031c0f3258b82f3a4fcab483
jan64 <jan64.email@gmail.com>
2014-09-07 20:27:06 +0200

    Update fly.lua
    Small fix

--- fly.lua
+++ fly.lua
@@ -1,18 +1,18 @@
 function HandleFlyCommand(Split, Player)
 	
+	local function ChangeFly( newPlayer )
+		newPlayer:SetCanFly(not newPlayer:CanFly());
+		newPlayer:SendMessageSuccess("Fly mode toggled!")
+		return true
+	end
+	
 	if(Split[2] == nil or Split[2] == Player:GetName()) then
-		Player:SetCanFly(not Player:CanFly());
-		Player:SendMessageSuccess("Fly mode toggled!")
+		ChangeFly(Player)
 	elseif (Player:HasPermission("core.fly.others")) then
 		
-		local function ChangeFly( newPlayer )
-			newPlayer:SetCanFly(not newPlayer:CanFly());
-			newPlayer:SendMessageSuccess("Fly mode toggled!")
+		if cRoot:Get():FindAndDoWithPlayer( Split[2], ChangeFly ) then
 			Player:SendMessageSuccess("Fly mode for player " .. Split[2] ..  " toggled!")
-			return true
-		end
-
-		if not cRoot:Get():FindAndDoWithPlayer( Split[2], ChangeFly ) then
+		else
 			SendMessageFailure( Player, "There was no player that matched your query." )
 		end
 		
