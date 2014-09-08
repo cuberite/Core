f078ea1b88c51360810e1aa7a55f7100243c5a89
madmaxoft <github@xoft.cz>
2014-09-08 15:53:54 +0200

    Implemented UUID-based banlist and IP banning.
    The data is stored in a SQLite DB.

--- onlogin.lua
+++ onlogin.lua
@@ -1,10 +1,5 @@
 function OnLogin(Client, ProtocolVersion, Username)
 	if( Username ~= "" ) then
-		if( BannedPlayersIni:GetValueB("Banned", Username, false) == true ) then
-			LOGINFO( Username .. " tried to join, but is banned!")
-			Client:Kick("You are banned!")
-			return true -- Player is banned, return true to deny access
-		end
 		if( WhiteListIni:GetValueB("WhiteListSettings", "WhiteListOn", false ) == true ) then
 			if( WhiteListIni:GetValueB("WhiteList", Username, false ) == false ) then -- not on whitelist
 				cRoot:Get():BroadcastChat(Username .. " tried to join, but is not on the whitelist.")
