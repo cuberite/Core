84ce8fa5c35ab0613028ec4202f47d95f554b715
Jonathan Fabian <jonfabe@github>
2014-11-29 20:56:22 -0500

    Fix issues from rebase

--- Info.lua
+++ Info.lua
@@ -558,12 +558,6 @@ g_PluginInfo =
 			HelpString = "Sends a chat message to all players",
 		},
 
-		["setversion"] =
-		{
-			Handler =  HandleConsoleVersion,
-			HelpString = "Sets server version reported to 1.4+ clients",
-		},
-
 		["time"] = 
 		{
 			Handler = HandleConsoleTime,
