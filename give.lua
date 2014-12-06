4b903cae7579277a66b01c4e2305ed292d594449
Jonathan Fabian <jonfabe@github>
2014-12-06 17:23:57 -0500

    Fixed handling of dataTag with spaces
    Also updated message handling, added comments.

--- give.lua
+++ give.lua
@@ -1,43 +1,57 @@
+-- Implements give, unsafegive, item, and unsafeitem commands and console commands
 
+
+-- Table to hold the processed data tag
 local DataTagTable = {}
 
-local UsageHead = "Usage: "
-local UsageTail = " <player> <item> [amount] [data] [dataTag]"
+local CommandUsage = "Usage: %s %s"
+local ItemCommandUsageTail = "<item> [amount] [data] [dataTag]"
+local GiveCommandUsageTail = "<PlayerName>" .. ItemCommandUsageTail
 
 local MessagePlayerFailure = "Player not found"
-local MessageAmountFailureHead = "The number you have entered ("
-local MessageAmountFailureTail = ") is too big, it must be at most 64"
-local MessageItemNameFailure = "There is no such item with name [ "
+local MessageAmountFailure = "The number you have entered ( %d ) is too big, it must be at most 64"
+local MessageItemNameFailure = "There is no such item with name [ %s ]"
 local MessageDataTagFailure = "Error processing dataTag: "
 local MessageUnknownError = "< Unknown Error >"
+local MessageGiveSuccessful = "Given [ %s ] x %d"
 
 local MaxNumberOfItems = 64
 
+
+--- Takes the given NBT DataTag and converts it to a table
+-- 
+--  @param DataTag String in the format of a minecraft NBT data tag
+--  
+--  @return True if successful, and false with an error message if a problem is encountered
+--  
 local function SplitDataTag( DataTag )
 
 	local table = {}
 
+	-- Modify the vanilla minecraft dataTag format to a lua table format
 	DataTag = string.gsub(DataTag,":","=")
 	DataTag = string.gsub(DataTag, "%[", "{")
 	DataTag = string.gsub(DataTag, "%]", "}")
 
-	local DataTagFunc, err = loadstring("dt = " .. DataTag)
+	-- Load the DataTag string into lua
+	local DataTagFunc, err = loadstring( "dt = " .. DataTag )
 	if not DataTagFunc then
 		return false, err
 	end
-
 	setfenv(DataTagFunc, table)
 	local Success, errMsg = pcall(DataTagFunc)
 	if not Success then
 		return false, errMsg
 	end
-	
+
 	DataTagTable = table.dt
 
 	return true, nil
 end
 
-
+--- Handle the `give` console and in-game command
+--  Usage: give <PlayerName> <item> [amount] [data] [dataTag]
+--  
 function HandleGiveCommand( Split, Player )
 
 	local PlayerName = Split[2]
@@ -49,7 +63,7 @@ function HandleGiveCommand( Split, Player )
 	local Damage  -- Need to find out how this is passed in the DataTag, maybe add a non-standard?
 
 	if not PlayerName or not ItemName or Amount < 1 or DataValue < 0 or DataValue > 15 then
-		local Message = UsageHead .. Split[1] .. UsageTail
+		local Message = string.format( CommandUsage, Split[1] , string.find( Split[1], "give" ) and GiveCommandUsageTail or ItemCommandUsageTail )
 		if Player then
 			SendMessage( Player, Message )
 		else
@@ -62,12 +76,14 @@ function HandleGiveCommand( Split, Player )
 	local Item = cItem()
 	local FoundItem = StringToItem( ItemName .. ":" .. DataValue, Item)
 
-	if not IsValidItem( Item.m_ItemType ) then  -- StringToItem does not check if item is valid
+	-- StringToItem does not check if item is valid
+	if not IsValidItem( Item.m_ItemType ) then
 		FoundItem = false
 	end
 
-	if not FoundItem  then
-		local Message = MessageItemNameFailure .. ItemName .. " ]"
+	-- If the Item doesn't exist, return and inform the caller
+	if not FoundItem then
+		local Message = string.format( MessageItemNameFailure, ItemName )
 		if Player then
 			SendMessageFailure( Player, Message )
 		else
@@ -76,9 +92,12 @@ function HandleGiveCommand( Split, Player )
 		return true
 	end
 
+	-- Process the DataTag information, if available
 	if Split[6] then
 
-		local Success, errMsg = SplitDataTag( Split[6] )
+		-- The DataTag values may be split across a few different indicies if it contains any spaces,
+		-- so concat the remainder of split together for processing
+		local Success, errMsg = SplitDataTag( table.concat( Split, " ", 6 ) )
 
 		if not Success then
 
@@ -92,20 +111,22 @@ function HandleGiveCommand( Split, Player )
 			return true
 		end
 
+		-- Set a custom name if given
 		if DataTagTable.display.Name then
 			Name = DataTagTable.display.Name
 			Item.m_CustomName = Name
 		end
 
+		-- Set Lore if given
 		if DataTagTable.display.Lore then
 			local Lore = ""
 			for _, value in ipairs(DataTagTable.display.Lore) do
-				LOG(value)
 				Lore = Lore .. value .. "`"  -- Newline is '`' character rather than "\n"
 			end
 			Item.m_Lore = Lore
 		end
 
+		-- Add enchantments, if given
 		if DataTagTable.ench then
 			for _, enchants in ipairs(DataTagTable.ench) do
 				Item.m_Enchantments:SetLevel(enchants.id,enchants.lvl)
@@ -114,8 +135,9 @@ function HandleGiveCommand( Split, Player )
 
 	end
 
+	-- Vanilla only allows a player to receive 64 items at a time, respecting that limit here
 	if Amount > MaxNumberOfItems then
-		local Message = MessageAmountFailureHead .. Amount .. MessageAmountFailureTail
+		local Message = string.format(MessageAmountFailure, Amount)
 		if Player then
 			SendMessageFailure( Player, Message )
 		else
@@ -124,6 +146,12 @@ function HandleGiveCommand( Split, Player )
 		return true
 	end
 
+	--- Callback function used to add items to a players inventory
+	--  
+	--  @param NewPlayer The cPlayer object representing the player to give the item to
+	--  
+	--  @return True if successful, false if the player cannot be found
+	--  
 	local function giveItems( NewPlayer )
 
 		if string.lower( NewPlayer:GetName() ) ~= lcPlayerName then
@@ -132,14 +160,14 @@ function HandleGiveCommand( Split, Player )
 
 		Item:AddCount(Amount - 1)
 		NewPlayer:GetInventory():AddItem( Item )
-		
-		local MessageHead = "Given [" .. Name or ItemToString( Item ) .. "] x " .. Amount
+
+		local MessageHead = string.format( MessageGiveSuccessful, ( Name or ItemToString( Item ) ), Amount )
 		local MessageTail = " to " .. NewPlayer:GetName()
 		SendMessageSuccess( NewPlayer, MessageHead )
 		if Player then
 			SendMessageSuccess( Player, MessageHead .. MessageTail )
 		end
-		LOG( Player and Player:GetName() or "Console" .. ": " .. MessageHead .. MessageTail )
+		LOG( ( Player and Player:GetName() or "Console" ) .. ": " .. MessageHead .. MessageTail )
 
 		return true
 	end
@@ -158,6 +186,9 @@ function HandleGiveCommand( Split, Player )
 end
 
 
+--- Handle the `item` in-game command
+--  Usage: item <item> [amount] [data] [dataTag]
+--  
 function HandleItemCommand( Split, Player )
 
 	table.insert( Split, 2, Player:GetName() )
