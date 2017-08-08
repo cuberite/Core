15aaf7de0bdebcd1509ce067a03242ac20754b01
William 'psyFi' Hatcher <william@hatcher.work>
2017-08-08 13:03:52 -0500

    Added console cmd & perms

--- Info.lua
+++ Info.lua
@@ -715,6 +715,24 @@ g_PluginInfo =
 			},
 		},
 
+		
+		["gamerule"]=
+		{
+			Handler = HandleConsoleGameRule,
+			HelpString = "Sets or queries a game rule value.",
+			ParameterCombinations =
+			{
+				{
+					Params = "rule",
+					Help = "Queries a game rule value",
+				},
+				{
+					Params = "rule value",
+					Help = "Sets a game rule value",
+				},
+			},
+		},
+
 		["give"] =
 		{
 			Handler =  HandleConsoleGive,
@@ -1111,6 +1129,12 @@ g_PluginInfo =
 		["core.enchant.self"] =
 		{
 			Description = "Allows players to add an enchantment to their own held item.",
+			RecommendedGroups = "admins",
+		},
+
+		["core.gamerule"] =
+		{
+			Description = "Allows players to set and query game rule values",
 			RecommendedGroups = "admins",
 		},
 
