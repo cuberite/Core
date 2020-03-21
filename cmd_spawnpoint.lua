e4bf7627a58344b1d4c9b4da5ab4a81b203e3a2e
Mat <mail@mathias.is>
2020-03-21 03:24:24 +0200

    Change behavior to match vanilla

--- cmd_spawnpoint.lua
+++ cmd_spawnpoint.lua
@@ -14,33 +14,25 @@ function HandleSpawnPointCommand(Split, Player)
 	end
 
 	local SetSpawnPoint = function(OtherPlayer)
-		OtherPlayer:SetBedPos(Vector3i(X, Y, Z), World)
+		if Split[5] then
+			if not Player then
+				X = OtherPlayer:GetPosX()
+				Y = OtherPlayer:GetPosY()
+				Z = OtherPlayer:GetPosZ()
+			end
 
-		Response = SendMessageSuccess(Player, "Set the spawn point of player \"" .. OtherPlayer:GetName() .. "\" to (" .. math.floor(X) .. ", " .. math.floor(Y) .. ", " .. math.floor(Z) .. ") in world \"" .. World:GetName() .. "\"")
-	end
-
-	if (not Player and not Split[5]) or (Split[3] and not Split[5]) then
-		Response = SendMessage(Player, "Usage: " .. Split[1] .. " <player> <x> <y> <z> [world]")
-	elseif not Split[2] then
-		SetSpawnPoint(Player)
-	else
-		if Split[3] then
 			X = RelativeCommandCoord(Split[3], X)
 
 			if not X then
 				return true, SendMessageFailure(Player, "'" .. Split[3] .. "' is not a valid number")
 			end
-		end
-		
-		if Split[4] then
+
 			Y = RelativeCommandCoord(Split[4], Y)
-			
+
 			if not Y then
 				return true, SendMessageFailure(Player, "'" .. Split[4] .. "' is not a valid number")
 			end
-		end
-		
-		if Split[5] then
+
 			Z = RelativeCommandCoord(Split[5], Z)
 
 			if not Z then
@@ -55,10 +47,18 @@ function HandleSpawnPointCommand(Split, Player)
 				return true, SendMessageFailure(Player, "Invalid world \"" .. Split[6] .. "\"")
 			end
 		end
-		
-		if Split[2] == "" or not cRoot:Get():FindAndDoWithPlayer(Split[2], SetSpawnPoint) then
-			Response = SendMessageFailure(Player, "Player \"" .. Split[2] .. "\" not found")
-		end
+
+		OtherPlayer:SetBedPos(Vector3i(X, Y, Z), World)
+
+		Response = SendMessageSuccess(Player, "Set the spawn point of player \"" .. OtherPlayer:GetName() .. "\" to (" .. math.floor(X) .. ", " .. math.floor(Y) .. ", " .. math.floor(Z) .. ") in world \"" .. World:GetName() .. "\"")
+	end
+
+	if (not Player and not Split[5]) or (Split[3] and not Split[5]) then
+		Response = SendMessage(Player, "Usage: " .. Split[1] .. " <player> <x> <y> <z> [world]")
+	elseif not Split[2] then
+		SetSpawnPoint(Player)
+	elseif Split[2] == "" or not cRoot:Get():FindAndDoWithPlayer(Split[2], SetSpawnPoint) then
+		Response = SendMessageFailure(Player, "Player \"" .. Split[2] .. "\" not found")
 	end
 	return true, Response
 end
