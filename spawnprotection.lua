e9b99582e10d5d3ca34297abbccb7b6d8429e10e
Mat <mail@mathias.is>
2020-03-24 13:51:45 +0200

    Improved spawn protection (#224)

---
+++ spawnprotection.lua
@@ -0,0 +1,68 @@
+
+-- Implements spawn protection for Cuberite
+
+local function IsInSpawn(X, Y, Z, WorldName)
+	local ProtectRadius = WorldsSpawnProtect[WorldName]
+	
+	if ProtectRadius > 0 then
+		local World = cRoot:Get():GetWorld(WorldName)
+		local SpawnArea = cBoundingBox(Vector3d(World:GetSpawnX() - ProtectRadius, -1000, World:GetSpawnZ() - ProtectRadius), Vector3d(World:GetSpawnX() + ProtectRadius, 1000, World:GetSpawnZ() + ProtectRadius))
+		local PlayerLocation = Vector3d(X, Y, Z)
+
+		if SpawnArea:IsInside(PlayerLocation) then
+			return true
+		end
+	end
+end
+
+local function CheckBlockModification(Player, BlockX, BlockY, BlockZ)
+	if not Player:HasPermission("core.build") then
+		SendMessageFailure(Player, "You do not have the \"core.build\" permission, thou cannot build")
+		return true
+	end
+
+	if not Player:HasPermission("core.spawnprotect.bypass") and IsInSpawn(BlockX, BlockY, BlockZ, Player:GetWorld():GetName()) then
+		SendMessageFailure(Player, "Go further from spawn to build")
+		return true
+	end
+end
+
+function OnBlockSpread(World, BlockX, BlockY, BlockZ, Source)
+	if Source == ssFireSpread and IsInSpawn(BlockX, BlockY, BlockZ, World:GetName()) then
+		return true
+	end
+		
+end
+
+function OnExploding(World, ExplosionSize, CanCauseFire, X, Y, Z, Source, SourceData)
+	if IsInSpawn(X, Y, Z, World:GetName()) then
+		return true
+	end
+end
+
+function OnPlayerBreakingBlock(Player, BlockX, BlockY, BlockZ, BlockFace, Status, OldBlockType, OldBlockMeta)
+	return CheckBlockModification(Player, BlockX, BlockY, BlockZ)
+end
+
+function OnPlayerPlacingBlock(Player, BlockX, BlockY, BlockZ, BlockFace, CursorX, CursorY, CursorZ, BlockType)
+	return CheckBlockModification(Player, BlockX, BlockY, BlockZ)
+end
+
+function OnPlayerRightClick(Player, BlockX, BlockY, BlockZ, BlockFace, CursorX, CursorY, CursorZ)
+	if not Player:HasPermission("core.spawnprotect.bypass") and IsInSpawn(BlockX, BlockY, BlockZ, Player:GetWorld():GetName()) then
+		if Player:GetWorld():GetBlock(BlockX, BlockY, BlockZ) == E_BLOCK_GRASS or
+				Player:GetWorld():GetBlock(BlockX, BlockY, BlockZ) == E_BLOCK_DIRT then
+			if Player:GetEquippedItem():IsEmpty() then
+				return false
+			end
+		end
+
+		if Player:GetEquippedItem().m_ItemType ~= E_ITEM_FLINT_AND_STEEL and
+				Player:GetEquippedItem().m_ItemType ~= E_ITEM_FIRE_CHARGE then
+			return false
+		end
+
+		SendMessageFailure(Player, "Go further from spawn to build")
+		return true
+	end
+end
