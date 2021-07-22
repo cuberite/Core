66093f250a1f6a5e32b092a53d3773eed7d551bf
mathiascode <mail@mathias.is>
2021-07-22 20:47:38 +0300

    Remove color codes from /listgroups and /listranks commands

--- cmd_listgroups.lua
+++ cmd_listgroups.lua
@@ -7,23 +7,17 @@ function HandleListGroupsCommand(Split, Player)
 
 	-- If no params are given, list all groups that the manager knows:
 	local RankName = Split[2]
-	local ResetColor = cChatColor.Plain
-
-	if not Player then
-		-- Color codes are not used in the console
-		ResetColor = ""
-	end
 
 	if not RankName then
 		-- Get all the groups:
 		local Groups = cRankManager:GetAllGroups()
 
-		Response = SendMessage(Player, "Available groups: " .. table.concat(Groups, ResetColor .. ", ") .. " (total: " .. #Groups .. ")")
+		Response = SendMessage(Player, "Available groups: " .. table.concat(Groups, ", ") .. " (total: " .. #Groups .. ")")
 	else
 		-- A rank name is given, list the groups in that rank:
 		local Groups = cRankManager:GetRankGroups(RankName)
 
-		Response = SendMessage(Player, "Groups in rank " .. RankName .. ": " .. table.concat(Groups, ResetColor .. ", ") .. " (total: " .. #Groups .. ")")
+		Response = SendMessage(Player, "Groups in rank " .. RankName .. ": " .. table.concat(Groups, ", ") .. " (total: " .. #Groups .. ")")
 	end
 	return true, Response
 end
--- cmd_listranks.lua
+++ cmd_listranks.lua
@@ -1,13 +1,6 @@
 function HandleListRanksCommand(Split, Player)
 	local Ranks = cRankManager:GetAllRanks()
-	local ResetColor = cChatColor.Plain
-
-	if not Player then
-		-- Color codes are not used in the console
-		ResetColor = ""
-	end
-
-	return true, SendMessage(Player, "Available ranks: " .. table.concat(Ranks, ResetColor .. ", ") .. " (total: " .. #Ranks .. ")")
+	return true, SendMessage(Player, "Available ranks: " .. table.concat(Ranks, ", ") .. " (total: " .. #Ranks .. ")")
 end
 
 function HandleConsoleListRanks(Split)
