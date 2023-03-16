f3d716df3fa73cd74034ba75b0799ef54504b431
Mat <mail@mathias.is>
2023-03-16 23:35:29 +0200

    cmd_version.lua: minor corrections

--- cmd_version.lua
+++ cmd_version.lua
@@ -1,13 +1,13 @@
 function HandleVersionCommand(Split, Player)
 	-- Send output to player:
-	SendMessage(Player, string.format(
+	Response = SendMessage(Player, string.format(
 		"This server is running Cuberite %s - %s (%s - %s) with Lua version %s and SQLite version %s.",
 		cRoot:GetBuildSeriesName(),
 		cRoot:GetBuildID(),
 		cRoot:GetBuildCommitID(),
 		cRoot:GetBuildDateTime(),
 		_VERSION,
-		sqlite3.version
+		sqlite3.version()
 	))
-	return true
+	return true, Response
 end
